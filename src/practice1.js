488f1badd28d16151f9e32e5d157605c074a2075
fightwing <18071766136@163.com>
2020-11-11 11:18:36 +0800

    finished practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) =>{
+    let data = input.data;
+    let column = input.column;
+    let output = [];
+    for( let i=0;i<data.length;i++){
+        let obj = {};
+        for(let j=0;j<data[i].length;j++){
+            obj[column[j].name] = data[i][j];
+        }
+        output.push(obj);
+    }
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
-export { inject };
+const inject = (items,sections)=>{
+    let output = [...items];
+     for(let i=0;i<sections.length;i++){
+         output.splice(sections[i].index+i,0,sections[i].content);
+     }
+     return output;
+}
+export {inject};
